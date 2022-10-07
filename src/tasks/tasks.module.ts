a618a9fa7a9cb20ab3242b4554dc97658cd36d5a
LucasSlrs <lucas.souleyrasse@gmail.com>
2022-10-07 09:22:44 +0100

    need to add datasource because of entityRepository is deprecated

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersRepository } from 'src/auth/users.repository';
 import { TasksController } from './tasks.controller';
 import { Task } from './tasks.entity';
 import { TaskRepository } from './tasks.repository';
 import { TasksService } from './tasks.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [TypeOrmModule.forFeature([Task, UsersRepository]), AuthModule],
   controllers: [TasksController],
   providers: [TasksService, TaskRepository],
 })
